815c5a2ab14469c9bd9b4b1b9189698aa157df91
volosincu <bogdan.volosincu@riseup.net>
2016-06-07 22:50:15 +0000

    v0.1 - color to be setted : --indicator --urgent-bg --urgent-border --urgent-text new version of the config file is save with timestamp and extension .file in current dir of the script

--- parse.sh
+++ parse.sh
@@ -1,17 +1,7 @@
 #!/bin/bash 
 
+#./parse.sh -f /home/sony/script/colorez/i3 --indicator='#cdcdcd' --urgent-bg='red' --urgent-border='blue' --urgent-text='green'
 
-#./parse.sh -btest --testt='spec' --unu un --alt nae
-
-
-
-
-#./parse.sh -b test --testt='spec' --unu un --alt='nae' --file='/home/sony/script/colorez/i3'
-
-#TEMP=`getopt -o -n 'parse.sh' -- "$@"`
-#eval set -- "$TEMP"
-
-#echo $@
 if [[ $# -gt 0 ]] ; then
    
   set -- `getopt -o f: --long indicator::,urgent-bg::,urgent-border::,urgent-text:: -n 'entering in parse.sh' -- "$@"`
@@ -24,24 +14,55 @@ if [[ $# -gt 0 ]] ; then
     invalid_params=`echo $params | awk 'BEGIN {FS="==";}{print $2 }'`
     echo -e "\nPlease provide the parameters" $invalid_params  " in the: \n\t 1) short form: -a, -a=\"value\" or \n\t 2) long form : --param-name, --param-name=\"value\""
   else
-    i=0 
-    while [ $# -gt 1 ];do
-      key=$1
-      val=$2
-      if [ $key == "-f" -o $key == "--file" ]; then
-        echo "reading config file : "$val
-        onfile=./i3-$(date +%s).file  
-	awk '{print $0}'  ${val:1:${#val}-2} > $onfile
-      fi 
-      
-      shift 2;
-      echo $key $val | awk '{print $1, $2}'
+
+    filex=2
+    for i in $@; do
+      if [ $i == "-f" -o $i == "--file" ]; then
+        cfgfile=${!filex}
+        cfgfile=${cfgfile:1:${#cfgfile}-2}
+      fi
+      ((filex++))
     done
+    
+    declare -a optsx argsx
+
+
+    j=0
+    while [ $# -gt 1 ];do
+      key=`echo $1 | sed -r 's/^(--|-)//'`
+      val=$2
+      shift 2
+      [ $key == "f" -o $key == "file" ] && continue
+      optsx[$j]=$key
+      argsx[$j]=${val:1:${#val}-2}
+      ((j++))
+    done
+
+
+
+    onfile=./i3-$(date +%s).file
+    awk -v opts="${optsx[*]}" -v args="${argsx[*]}" '
+
+         {
+           split(opts,axopts ," ");
+           lngth=split(args,axargs ," ");
+            
+           for (i=1;i<=lngth;i++){
+             rgx="^(set)[[:blank:]]+\\$" axopts[i]
+             if(match($0, rgx)){
+               where=match($0, "(#[a-zA-Z0-9]{6})"); 
+               if(where){ 
+                  st = substr($0, RSTART, RLENGTH) ; 
+                  gsub(st , axargs[i], $0);
+               }
+
+             }
+           }
+           print $0
+         } # endbody
+         END { }' $cfgfile > $onfile
   fi
 else
   echo "Please supply at least an parameter!"
 fi
-
-
-#echo "&&& "$@" &&&"
 
