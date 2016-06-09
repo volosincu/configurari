63e2a32caaa1249c8aa8e603330a819d56efebbc
volosincu <bogdan.volosincu@riseup.net>
2016-06-09 14:55:39 +0000

    add more color variables for windows and bar

--- i3-color-tool.sh
+++ i3-color-tool.sh
@@ -1,10 +1,53 @@
 #!/bin/bash 
 
-#./parse.sh -f /home/sony/script/colorez/i3 --indicator='#cdcdcd' --urgent-bg='red' --urgent-border='blue' --urgent-text='green'
 
-if [[ $# -gt 0 ]] ; then
-   
-  set -- `getopt -o f: --long indicator:,urgent-bg:,urgent-border:,urgent-text: -n 'entering in parse.sh' -- "$@"`
+
+#  ./i3-color-tool.sh \
+#   -f /home/sony/.config/i3/config \
+#   --indicator='#FF0040' \
+#   --urgent-bg='#E53935' \
+#   --urgent-border='#E53935' \
+#   --urgent-text='#f3f4f5' \
+#   --wn-focused-border='#49626d' \
+#   --wn-focused-bg='#49626d' \
+#   --wn-focused-text='#ffffff' \
+#   --wn-inactive-border='#35393c' \
+#   --wn-inactive-bg='#35393c' \
+#   --wn-inactive-text='#c0c2c8' \
+#   --bar-bg='#2f343f' \
+#   --bar-separator='#757575' \
+#   --bar-focus-ws-border='#c37654' \
+#   --bar-focus-ws-bg='#c37654' \
+#   --bar-focus-ws-text='#ffffff' \
+#   --bar-inactive-ws-border='#373b41' \
+#   --bar-inactive-ws-bg='#373b41' \
+#   --bar-inactive-ws-text='#7d7f7d'
+
+if [[ $# == 0 ]] ; then
+  echo "Please supply at least an parameter!"
+  exit
+fi
+
+
+set -- `getopt -o f: --long \
+indicator:,\
+urgent-bg:,\
+urgent-border:,\
+urgent-text:,\
+wn-focused-border:,\
+wn-focused-bg:,\
+wn-focused-text:,\
+wn-inactive-border:,\
+wn-inactive-bg:,\
+wn-inactive-text:,\
+bar-bg:,\
+bar-separator:,\
+bar-focus-ws-border:,\
+bar-focus-ws-bg:,\
+bar-focus-ws-text:,\
+bar-inactive-ws-border:,\
+bar-inactive-ws-bg:,\
+bar-inactive-ws-text -n 'entering in parse.sh' -- "$@"`
   
 
   params=`echo $@ | awk 'BEGIN {}{gsub(/--[^[:alnum:]]/, "==");  print $0 }'`
@@ -25,7 +68,8 @@ if [[ $# -gt 0 ]] ; then
     done
     
     declare -a optsx argsx
-
+    
+    # TODO - change code to iterate throught arguments without deleting them
     # add options and their arguments in separate arrays 
     j=0
     while [ $# -gt 1 ];do
@@ -62,7 +106,4 @@ if [[ $# -gt 0 ]] ; then
          } 
          END { }' $cfgfile > $onfile
   fi
-else
-  echo "Please supply at least an parameter!"
-fi
 
